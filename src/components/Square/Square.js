f8d568765ad83bdb3710b748520371766f298ca4
tom-waters-myob <tom.waters@myob.com>
2021-01-07 11:16:43 +1100

    added onclick toggle to square component selection

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,8 +1,19 @@
 import './Square.css';
+import React, { useState } from 'react';
 
 function Square(props) {
+    const [selected, setSelected] = useState(false);
+
+    function toggleSquareSelect() {
+        if (selected) { setSelected(false) }
+        else { setSelected(true) }
+    }
+
     return (
-        <div className={props.class}>
+        <div
+            className={`${props.class} ${selected ? "selected" : ""}`}
+            onClick={() => toggleSquareSelect()}
+        >
             {props.question}
         </div>
     );
